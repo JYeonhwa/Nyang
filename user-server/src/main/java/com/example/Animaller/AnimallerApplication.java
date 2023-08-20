3fb57f712c984bd2870b002ea4df2db8b936b3cf
정연화 <laboratory6889@gmail.com>
2023-08-20 12:04:03 +0900

    :art: server 구조 변경

---
+++ user-server/src/main/java/com/example/Animaller/AnimallerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Animaller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimallerApplication.class, args);
+	}
+
+}
